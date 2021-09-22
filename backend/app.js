e3c8b0e6a1deebae3f97f7dfc197d11b13e97cd1
Mattias Törnqvist <mtoxr09@student.lnu.se>
2021-09-22 21:20:39 +0200

    Added cloud MongoDB with post and get functionality

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,78 @@
+const { APP_ID } = require('@angular/core')
 const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+
+const Post = require('./models/post')
 
 const app = express()
 
-app.use((req, res, next) => {
-  console.log('First middleware!')
+mongoose.connect('mongodb+srv://Max:5KQ3Dfd2MZgBGb3r@cluster0.zwpgm.mongodb.net/node-angular?retryWrites=true&w=majority')
+.then(() => {
+  console.log('Connected to cloud database successfully!')
+})
+.catch(() => {
+  console.log('Connection to cloud database failed!')
+})
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.use("/api/posts", (req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*")
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  )
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE, OPTIONS"
+  )
   next()
 })
 
-app.use((req, res, next) => {
-  res.send('Hello from Express!')
+app.post("/api/posts", (req, res, next) => {
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content
+  })
+  post.save()
+  res.status(201).json({
+    message: "Post added successfully!"
+  })
 })
 
+app.get("/api/posts", (req, res, next) => {
+  Post.find().then(documents => {
+    console.log(documents)
+    res.status(200).json({
+      message: "Posts fetched successfully!",
+      posts: documents
+    })
+  })
+
+})
+
+/*
+app.use("/api/posts", (req, res, next) => {
+
+const posts = [
+  {
+    id: "isdfjsdfsdf",
+    title: "First post!",
+    content: "First content!"
+  },
+  {
+    id: "isdfjsdfsdf",
+    title: "Second post!",
+    content: "Second content!"
+  }
+]
+
+  res.status(200).json({
+    message: 'Posts fetched succesfully',
+    posts: posts
+  })
+})
+*/
 module.exports = app
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -1,14 +1,31 @@
 import { Injectable } from "@angular/core";
 import { Subject } from "rxjs";
 import { Post } from './post.model'
+import { HttpClient } from '@angular/common/http'
+import { map } from 'rxjs/operators'
 
 @Injectable({providedIn: "root"})
 export class PostsService {
   private posts: Post[] = []
   private postsUpdated = new Subject<Post[]>()
 
+  constructor(private http: HttpClient) {}
+
   getPosts() {
-    return [...this.posts]
+    this.http.get<{message: string, posts: any}>('http://localhost:3000/api/posts')
+    .pipe(map((postData) => {
+      return postData.posts.map(post => {
+        return {
+          title: post.title,
+          content: post.content,
+          id: post._id
+        }
+      })
+    }))
+    .subscribe(transformedPosts => {
+      this.posts = transformedPosts
+      this.postsUpdated.next([...this.posts])
+    })
   }
 
   getPostUpdateListener() {
@@ -16,8 +33,11 @@ export class PostsService {
   }
 
   addPosts(title: string, content: string) {
-    const post: Post = {title: title, content: content}
-    this.posts.push(post)
-    this.postsUpdated.next([...this.posts])
+    const post: Post = {id: null, title: title, content: content}
+    this.http.post<{ message: string }>("http://localhost:3000/api/posts", post).subscribe((responseData => {
+      console.log(responseData.message)
+      this.posts.push(post)
+      this.postsUpdated.next([...this.posts])
+    }))
   }
 }

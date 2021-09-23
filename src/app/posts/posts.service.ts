02f1457ab8a79e4e82c0edbf8b89e50099c3e82e
Mattias Törnqvist <mtoxr09@student.lnu.se>
2021-09-23 21:27:24 +0200

    fetch post id from db when creating a post

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -34,8 +34,9 @@ export class PostsService {
 
   addPosts(title: string, content: string) {
     const post: Post = {id: null, title: title, content: content}
-    this.http.post<{ message: string }>("http://localhost:3000/api/posts", post).subscribe((responseData => {
-      console.log(responseData.message)
+    this.http.post<{ message: string, postId: string }>("http://localhost:3000/api/posts", post).subscribe((responseData => {
+      const id = responseData.postId
+      post.id = id
       this.posts.push(post)
       this.postsUpdated.next([...this.posts])
     }))

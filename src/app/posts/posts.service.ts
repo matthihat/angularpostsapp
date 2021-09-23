b67db541057af66cdb2590f7e6af94d4a8e91492
Mattias Törnqvist <mtoxr09@student.lnu.se>
2021-09-23 21:12:33 +0200

    Update front-end when post is deleted

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -44,7 +44,11 @@ export class PostsService {
   deletePost(postId) {
     this.http.delete("http://localhost:3000/api/posts/" + postId)
     .subscribe(() => {
-      console.log("Deleted!")
+      const updatedPosts = this.posts.filter(post =>
+        post.id !== postId
+      )
+      this.posts = updatedPosts
+      this.postsUpdated.next([...this.posts])
     })
   }
 }
